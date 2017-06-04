18479601fcd4688ae3426b45fe30dd18a2cae53f
Diane Losardo <dlosardo@gmail.com>
2017-06-04 16:40:56 -0400

    fixed setup.py to correct install requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements():
     """Reads the installation requirements from requirements.pip"""
     with open("requirements.pip") as f:
         lines = f.read().split("\n")
-        lines_without_comments = filter(lambda l: not l.startswith('#'), lines)
+        lines_without_comments = list(filter(lambda l: not l.startswith('#'), lines))
         return lines_without_comments
 
 here = path.abspath(path.dirname(__file__))
@@ -25,7 +25,7 @@ setup(name='machine_learning',
     download_url='todo',
     author_email='dlosardo@gmail.com',
     version='0.1',
-    #install_requires=get_requirements(),
+    install_requires=get_requirements(),
     packages=['machine_learning'],
     package_dir={'machine_learning': ''},
     scripts=['bin/run.py']
